e97c1e8ac9961dda8d1295874c2bdaf00942db78
Ubuntu <ubuntu@ip-172-26-8-191.ap-northeast-2.compute.internal>
2019-06-24 04:57:17 +0000

    config_fix

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ $config = array(
 	"ubi-email" => "jhj777_@naver.com",
 	"ubi-password" => "r6sparser",
 	"default-region" => "emea", //Regions: emea - ncsa - apac 
-	"default-stats" => "casualpvp_death,casualpvp_kdratio,casualpvp_kills", 
+	"default-stats" => "casualpvp_death,casualpvp_kills,casualpvp_matchlost,casualpvp_matchplayed,casualpvp_matchwon,casualpvp_timeplayed,rankedpvp_death,rankedpvp_kills,rankedpvp_matchlost,rankedpvp_matchplayed,rankedpvp_matchwon,rankedpvp_timeplayed",
 	//All stats: https://gist.github.com/LaxisB/3924cfdc35562b719d1c891cdb895366
 	"default-platform" => "uplay", //Platforms: uplay - xbl - psn
 	"default-progression" => "true" //Enabled: true - false
